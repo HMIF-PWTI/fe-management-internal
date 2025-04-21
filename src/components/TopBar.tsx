5854b7afb417553b5b7b4f5d71148ed7caafaca3
Rifqi1012 <rifqimh1210@gmail.com>
2025-04-22 01:16:02 +0700

    [UPDATE]: HEHE

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,15 +1,19 @@
 import { useState, useRef, useEffect } from "react";
-import { matchPath, useLocation } from "react-router-dom";
+import { matchPath, useLocation, useNavigate } from "react-router-dom";
 import { MdPerson } from "react-icons/md";
 import Button from "./Button";
 import { AiOutlineLogout } from "react-icons/ai";
 import { getToko } from "@/service/Toko";
+import Swal from "sweetalert2";
+import { postLogout } from "@/service/Login";
 
 const TopBar = () => {
   const location = useLocation();
   const [showDropdown, setShowDropdown] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const [namaToko, setNamaToko] = useState("");
+  const navigate = useNavigate();
+  const nama = sessionStorage.getItem("nama");
 
   useEffect(() => {
     const fetchToko = async () => {
@@ -136,6 +140,46 @@ const TopBar = () => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
+  const handleLogout = async () => {
+    const confirm = await Swal.fire({
+      title: "Konfirmasi Logout",
+      text: "Apakah Anda yakin ingin keluar?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Ya, keluar",
+      cancelButtonText: "Batal",
+      reverseButtons: true,
+    });
+
+    if (!confirm.isConfirmed) return;
+
+    const token = sessionStorage.getItem("token");
+
+    if (!token) {
+      Swal.fire("Oops", "Token tidak ditemukan", "error");
+      return;
+    }
+
+    try {
+      await postLogout(token);
+      sessionStorage.clear();
+
+      await Swal.fire({
+        icon: "success",
+        title: "Logout berhasil!",
+        showConfirmButton: true,
+        customClass: {
+          popup: "custom-popup",
+        },
+      });
+
+      navigate("/");
+    } catch (error) {
+      console.error("Logout gagal:", error);
+      Swal.fire("Gagal", "Terjadi kesalahan saat logout", "error");
+    }
+  };
+
   return (
     <div className="flex items-center justify-between p-4 border-b border-dark-tertiary bg-dark-primary relative">
       <h1 className="text-xl font-semibold text-gold">{getPageTitle()}</h1>
@@ -148,11 +192,12 @@ const TopBar = () => {
         </button>
         {showDropdown && (
           <div className="animate-slide-in absolute p-3 rounded-lg bg-dark-secondary right-0 mt-4 w-52 space-y-5 z-50">
-            <h1>Hello Admin</h1>
+            <h1>Hello {nama}</h1>
             <Button
               variant="outline"
               icon={<AiOutlineLogout className="text-lg" />}
               iconPosition="right"
+              onClick={handleLogout}
             >
               Logout
             </Button>
